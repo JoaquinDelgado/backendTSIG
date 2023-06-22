8c3b8e58158fd92e611df1435bf1c2f6ea8e8db7
Fernando Maldonado <fernando.maldonado@fing.edu.uy>
2023-06-22 17:49:50 -0300

    Update readme.md

--- tsig/scripts/initDB.sql
+++ tsig/scripts/initDB.sql
@@ -77,3 +77,36 @@ CREATE TABLE IF NOT EXISTS public.cache_sugerencias (
   response character varying COLLATE pg_catalog."default",
   fecha_creado timestamp
 );
+
+CREATE TABLE IF NOT EXISTS public.direcciones_im
+(
+    id serial PRIMARY key,
+    calle character varying COLLATE pg_catalog."default",
+    numero integer,
+    latitud double precision,
+    longitud double precision
+);
+
+CREATE TABLE IF NOT EXISTS public.distancias_im_geocoders
+(
+    id serial PRIMARY key,
+    id_direcciones_im integer,
+    id_geocoder integer,
+    latitud_geocoder double precision,
+    longitud_geocoder double precision,
+    distancia_metros double precision
+);
+
+INSERT INTO public.direcciones_im(
+	calle, numero, latitud, longitud)
+	VALUES 
+	('Av. Gonzalo Ramírez', 1270, -34.911815424412055, -56.187992386335544),
+	('Av. Uruguay', 1936, -34.89861649270423, -56.17652060352443),
+	('Av. Santiago Rivas', 1500, -34.89793079168539, -56.13218848818313),
+	('Marsella', 2738, -34.87615263134674,-56.1838250881837),
+	('Av. Agraciada', 4241, -34.85671219459859,-56.221153130513486),
+	('Haití', 1606, -34.87180262558506,-56.250478601677884),
+	('Av. Carlos María Ramírez', 881, -34.86592307702279,-56.23571257711002),
+	('Soria', 1243, -34.83771136172423,-56.1991293016787),
+	('Camino Tomkinson', 2459, -34.8373196610002,-56.2731408350664),
+	('Hipólito Yrigoyen', 2069, -34.88062865081787,-56.111827816231106);
